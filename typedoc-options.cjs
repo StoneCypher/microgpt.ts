45eada7d59f0e795dbed7693063d99df5d5d12b0
John Haugeland <stonecypher@gmail.com>
2026-03-02 09:40:36 -0800

    install in gh action, add typedoc

---
+++ typedoc-options.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'MicroGPT.ts - a conversion of Karpathy\'s MicroGPT to Typescript',
+  readme: './README.md',
+  out: 'docs/docs',
+  customCss: './src/html/typedoc-extra.css',
+  excludePrivate: true
+};
